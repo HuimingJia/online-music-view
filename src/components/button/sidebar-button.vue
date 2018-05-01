3df3fafcf456e65870bdde09c8e6ec30dd15cf55
Huiming Jia <jiahm92@unibit.ai>
2018-05-01 10:14:47 -0400

    delete stuff

---
+++ src/components/button/sidebar-button.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+<div class="side-bar-btn">
+  <v-icon :name="icon"></v-icon>
+  <router-link :to="{ name: '', params: {} }">{{text}}</router-link>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: null
+    },
+    text: {
+      type: String,
+      default: null
+    },
+    link: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.side-bar-btn {
+  display: flex;
+  flex-direction: row;
+}
+
+.v-icon {
+  height: 24px;
+}
+</style>
---
+++ src/components/side-bar.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+<div id="sidebar">
+<h5>Explore</h5>
+<hr></hr>
+<sidebar-button text="For Me" icon="" link=""></sidebar-button>
+<sidebar-button text="Billboard" icon="" link=""></sidebar-button>
+<sidebar-button text="Singers" icon="" link=""></sidebar-button>
+<!-- <ul>
+  <li>For-Me</li>
+  <li>Billboard</li>
+  <li>Singers</li>
+</ul> -->
+</div>
+</template>
+
+<script>
+import SidebarButton from '@/components/button/sidebar-button.vue'
+export default {
+  components: {
+    SidebarButton
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#sidebar {
+  width: 200px;
+  background-color: rgb(0,0,0,0.5);
+}
+</style>
